d3756ecc7cdd29a627007c5dc7915217668824c9
Caleb Eby <caleb.eby01@gmail.com>
2022-07-01 19:33:56 -0700

    Add coveralls

--- packages/severed/vite.config.ts
+++ packages/severed/vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     includeSource: ['**.ts'],
     coverage: {
-      reporter: ['text', 'cobertura'],
+      reporter: ['text', 'cobertura', 'lcov'],
     },
   },
 });
